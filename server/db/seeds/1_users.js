7ecccb11466663c0784dab497a00b11671bb01bb
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-11-30 21:14:37 +1300

    Change user 2 to a client

--- server/db/seeds/1_users.js
+++ server/db/seeds/1_users.js
@@ -2,17 +2,17 @@ export async function seed(knex) {
   await knex('users').insert([
     {
       id: 'auth0|001',
-      username: 'banana',
+      username: 'bananaClient',
       name: 'Banana Cabana',
       email: 'banana@example.org',
       is_admin: false,
     },
     {
       id: 'auth0|002',
-      username: 'apple',
+      username: 'appleClient',
       name: 'Apple Berapple',
       email: 'apple@example.org',
-      is_admin: true,
+      is_admin: false,
     },
     {
       id: 'auth0|999',
