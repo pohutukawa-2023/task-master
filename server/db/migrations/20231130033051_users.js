51ffb43a25d86166efc7834ed13fab1987a1b54c
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-11-30 17:07:11 +1300

    Add db migrations and seeds
    For tables: users, task_options, and tasks

---
+++ server/db/migrations/20231130033051_users.js
@@ -0,0 +1,13 @@
+export async function up(knex) {
+  await knex.schema.createTable('users', (table) => {
+    table.increments('id').primary()
+    table.string('username').unique()
+    table.string('name')
+    table.string('email')
+    table.boolean('is_admin')
+  })
+}
+
+export async function down(knex) {
+  await knex.schema.dropTable('users')
+}
--- server/db/seeds/0_clean.js
+++
@@ -1,3 +0,0 @@
-export async function seed(knex) {
-  await knex('fruits').del()
-}
