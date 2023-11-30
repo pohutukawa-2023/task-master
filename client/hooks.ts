af072ba2ad17702eda2b83f98faa38804cc8f465
Pathik Modi <modipathik95@gmail.com>
2023-11-30 17:59:01 +1300

    replace fruits to tasks on client side

--- client/hooks.ts
+++ client/hooks.ts
@@ -3,43 +3,43 @@ import { useMutation, useQueryClient } from '@tanstack/react-query'
 
 import * as API from './api.ts'
 
-export function useFruits() {
-  const query = useQuery({
-    queryKey: ['fruits'],
-    queryFn: API.getFruits,
-  })
+// export function useFruits() {
+//   const query = useQuery({
+//     queryKey: ['fruits'],
+//     queryFn: API.getFruits,
+//   })
 
-  return {
-    ...query,
-    update: useUpdateFruit(),
-    delete: useDeleteFruit(),
-    add: useAddFruit(),
-  }
-}
+//   return {
+//     ...query,
+//     update: useUpdateFruit(),
+//     delete: useDeleteFruit(),
+//     add: useAddFruit(),
+//   }
+// }
 
-export function useFruitMutation<TData = unknown, TVariables = unknown>(
-  mutationFn: MutationFunction<TData, TVariables>
-) {
-  const queryClient = useQueryClient()
+// export function useFruitMutation<TData = unknown, TVariables = unknown>(
+//   mutationFn: MutationFunction<TData, TVariables>
+// ) {
+//   const queryClient = useQueryClient()
 
-  const mutation = useMutation({
-    mutationFn,
-    onSuccess: () => {
-      queryClient.invalidateQueries(['fruits'])
-    },
-  })
+//   const mutation = useMutation({
+//     mutationFn,
+//     onSuccess: () => {
+//       queryClient.invalidateQueries(['fruits'])
+//     },
+//   })
 
-  return mutation
-}
+//   return mutation
+// }
 
-export function useUpdateFruit() {
-  return useFruitMutation(API.updateFruit)
-}
+// export function useUpdateFruit() {
+//   return useFruitMutation(API.updateFruit)
+// }
 
-export function useDeleteFruit() {
-  return useFruitMutation(API.deleteFruit)
-}
+// export function useDeleteFruit() {
+//   return useFruitMutation(API.deleteFruit)
+// }
 
-export function useAddFruit() {
-  return useFruitMutation(API.addFruit)
-}
+// export function useAddFruit() {
+//   return useFruitMutation(API.addFruit)
+// }
--- server/server.ts
+++ server/server.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import * as Path from 'node:path'
 import * as URL from 'node:url'
 
-import fruitRoutes from './routes/tasks.ts'
+import taskRoutes from './routes/tasks.ts'
 
 const __filename = URL.fileURLToPath(import.meta.url)
 const __dirname = Path.dirname(__filename)
@@ -12,7 +12,7 @@ const server = express()
 server.use(express.json())
 server.use(express.static(Path.join(__dirname, 'public')))
 
-server.use('/api/v1/tasks', fruitRoutes)
+server.use('/api/v1/tasks', taskRoutes)
 
 server.get('*', (req, res) => {
   res.sendFile(Path.join(__dirname, 'public/index.html'))
