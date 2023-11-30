a1d61220afaaaba4ebbb2dab3d58addab82eb4b2
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-12-01 11:11:41 +1300

    Add auth to API routes

--- server/routes/clients.test.ts
+++ server/routes/clients.test.ts
@@ -3,6 +3,8 @@ import request from 'supertest'
 import server from '../server'
 import * as db from '../db/users'
 
+import { getMockToken } from './mockToken'
+
 vi.mock('../db/users')
 
 describe('GET /api/v1/client/:auth0id', () => {
@@ -18,24 +20,27 @@ describe('GET /api/v1/client/:auth0id', () => {
     ]
 
     vi.mocked(db.getUser).mockResolvedValue(testClient)
-    const response = await request(server).get('/api/v1/client/auth0|001')
-    // .set('authorization', `Bearer ${getMockToken()}`)
+    const response = await request(server)
+      .get('/api/v1/client/auth0|001')
+      .set('authorization', `Bearer ${getMockToken()}`)
     expect(response.status).toBe(200)
     expect(response.body).toEqual(testClient)
   })
 
   it('should return 404 if the users not found', async () => {
     vi.mocked(db.getUser).mockResolvedValue([])
-    const response = await request(server).get('/api/v1/client/auth0|001')
-    // .set('authorization', `Bearer ${getMockToken()}`)
+    const response = await request(server)
+      .get('/api/v1/client/auth0|001')
+      .set('authorization', `Bearer ${getMockToken()}`)
     expect(response.status).toBe(404)
     expect(response.text).toEqual('Not found')
   })
 
   it('should return 500 if the promise is rejected', async () => {
     vi.mocked(db.getUser).mockRejectedValue(new Error())
-    const response = await request(server).get('/api/v1/client/auth0|001')
-    // .set('authorization', `Bearer ${getMockToken()}`)
+    const response = await request(server)
+      .get('/api/v1/client/auth0|001')
+      .set('authorization', `Bearer ${getMockToken()}`)
     expect(response.status).toBe(500)
     expect(response.text).toEqual('Something went wrong')
   })
--- server/routes/clients.ts
+++ server/routes/clients.ts
@@ -1,12 +1,12 @@
 import express from 'express'
-// import { JwtRequest } from '../auth0.ts'
 
 import * as db from '../db/users.ts'
+import { validateAccessToken } from '../auth0'
 
 const router = express.Router()
 
 // GET /api/v1/client
-router.get('/:auth0id', async (req, res) => {
+router.get('/:auth0id', validateAccessToken, async (req, res) => {
   const auth0id = req.params.auth0id
 
   try {
