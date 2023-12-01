e2c71dc3c3ed620f19b32e6b018d208ff3f0c62f
Kirsty Ammundsen <kirstyammundsen@hotmail.com>
2023-12-01 13:21:26 +1300

    create home page layout (iphone pro 12)

--- client/routes.tsx
+++ client/routes.tsx
@@ -1,10 +1,10 @@
 import { Route, createRoutesFromElements } from 'react-router-dom'
 
-import AppLayout from './components/AppLayout.tsx'
-import Fruits from './components/Fruits.tsx'
+import AppLayout from './Pages/AppLayout.tsx'
+import Home from './Pages/Home.tsx'
 
 export const routes = createRoutesFromElements(
   <Route path="/" element={<AppLayout />}>
-    <Route index element={<Fruits />} />
+    <Route index element={<Home />} />
   </Route>
 )
