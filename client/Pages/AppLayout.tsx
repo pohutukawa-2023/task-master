fa0fe76d28da99f195ae5be0fc7ca2bbe58dd2b3
xavier-kirkpatrick <kirkpatrickxavier@gmail.com>
2023-12-01 16:59:08 +1300

    commit to merge with main

--- client/Pages/AppLayout.tsx
+++ client/Pages/AppLayout.tsx
@@ -1,8 +1,12 @@
 import { Outlet } from 'react-router-dom'
+import Nav from '../components/Nav'
 
 export default function AppLayout() {
   return (
     <>
+      <div>
+        <Nav />
+      </div>
       <main>
         <Outlet />
       </main>
--- server/routes/admins.ts
+++ server/routes/admins.ts
@@ -1,13 +1,14 @@
 import express from 'express'
 import { getTasksByAdmin } from '../db/getTasks'
+import { validateAccessToken } from '../auth0'
 
 const router = express.Router()
 
-router.get('/:clientId/tasks', async (req, res) => {
-  const adminId = req.params.adminId
+router.get('/:clientId/tasks', validateAccessToken, async (req, res) => {
+  const adminId = req.auth?.payload.sub
   const clientId = req.params.clientId
   try {
-    const result = await getTasksByAdmin(adminId, clientId)
+    const result = await getTasksByAdmin(adminId as string, clientId)
     if (!result) {
       return res.status(404).send('Not found')
     } else {
