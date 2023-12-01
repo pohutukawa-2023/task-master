ebe21a6f0be163dbed092ada8549aac70ee2347c
Pathik Modi <modipathik95@gmail.com>
2023-12-01 13:05:25 +1300

    testing server routes

--- server/db/users.ts
+++ server/db/users.ts
@@ -6,3 +6,7 @@ export async function getUser(id: User['id']): Promise<User[]> {
     .select('username', 'name', 'email', 'is_admin')
     .where('id', id)
 }
+
+export async function getAdminClients(authId: string) {
+  return await db('tasks').select().where('admin_id', authId)
+}
