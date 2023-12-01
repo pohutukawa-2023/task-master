134ce8ac52032594feb3e2d50e7d23e9b2849c55
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-12-01 21:49:41 +1300

    Ensure user is authenticated

--- client/routes.tsx
+++ client/routes.tsx
@@ -4,11 +4,15 @@ import AppLayout from './Pages/AppLayout.tsx'
 import Profile from './Pages/Profile.tsx'
 import Home from './Pages/Home.tsx'
 import AdminClientlist from './Pages/AdminClientlist/AdminClientlist.tsx'
+import ProtectedComponent from './components/UI/ProtectedComponent.tsx'
 
 export const routes = createRoutesFromElements(
   <Route path="/" element={<AppLayout />}>
     <Route index element={<Home />} />
-    <Route path="/profile" element={<Profile />} />
+    <Route
+      path="/profile"
+      element={<ProtectedComponent component={Profile} />}
+    />
     <Route path="/admin/clientlist" element={<AdminClientlist />} />
   </Route>
 )
