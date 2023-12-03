4d1fd2b9d4b595e23cdd73cd7540edfb31024c5c
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-12-03 14:38:23 +1300

    Add insertTask db function

---
+++ types/Task.ts
@@ -0,0 +1,17 @@
+import * as z from 'zod'
+
+export const taskDraftSchema = z.object({
+  userId: z.string(),
+  adminId: z.string(),
+  taskOptionId: z.number(),
+  data: z.string(),
+  isComplete: z.boolean(),
+  date: z.date(),
+})
+
+export const taskSchema = taskDraftSchema.extend({
+  id: z.number(),
+})
+
+export type TaskDraft = z.infer<typeof taskDraftSchema>
+export type Task = z.infer<typeof taskSchema>
