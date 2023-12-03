efd606cd561e22e77b81732e771c51eba3c67a6c
Pathik Modi <modipathik95@gmail.com>
2023-12-04 10:10:50 +1300

    fix nav bar if admin commented out

--- client/components/BottomNav.tsx
+++ client/components/BottomNav.tsx
@@ -1,34 +1,31 @@
-import { IfAdmin, IfNotAdmin } from './Authenticated.tsx'
+// import { IfAdmin, IfNotAdmin } from './Authenticated.tsx'
 import NavButton from './UI/NavButton/NavButton.tsx'
 import { Link } from 'react-router-dom'
 
 function BottomNav() {
   return (
     <>
-      <IfNotAdmin>
-        <div className="my-4 w-full md:w-fit flex justify-around absolute inset-x-0 bottom-0">
-          <Link to="/profile">
-            <NavButton>
-              <img
-                src={`./images/png/007-user-1.png`}
-                alt="profile user icon"
-              />
-            </NavButton>
-          </Link>
-          <Link to="/test">
-            <NavButton>
-              <img src={`./images/png/013-task-1.png`} alt="tasks icon" />
-            </NavButton>
-          </Link>
-          <Link to="/test">
-            <NavButton>
-              <img src={`./images/png/001-bar-chart.png`} alt="stats icon" />
-            </NavButton>
-          </Link>
-        </div>
-      </IfNotAdmin>
+      {/* <IfNotAdmin> */}
+      <div className="my-4 w-full md:w-fit flex justify-around absolute inset-x-0 bottom-0">
+        <Link to="/profile">
+          <NavButton>
+            <img src={`./images/png/007-user-1.png`} alt="profile user icon" />
+          </NavButton>
+        </Link>
+        <Link to="/test">
+          <NavButton>
+            <img src={`./images/png/013-task-1.png`} alt="tasks icon" />
+          </NavButton>
+        </Link>
+        <Link to="/test">
+          <NavButton>
+            <img src={`./images/png/001-bar-chart.png`} alt="stats icon" />
+          </NavButton>
+        </Link>
+      </div>
+      {/* </IfNotAdmin> */}
 
-      <IfAdmin>
+      {/* <IfAdmin>
         <div className="my-6 w-full md:w-fit flex justify-around absolute inset-x-0 bottom-0">
           <Link to="/profile">
             <NavButton>
@@ -54,7 +51,7 @@ function BottomNav() {
             </NavButton>
           </Link>
         </div>
-      </IfAdmin>
+      </IfAdmin> */}
     </>
   )
 }
