5df4a517d9dfea76773c9e4e2e91ba7194d5c923
Kirsty Ammundsen <kirstyammundsen@hotmail.com>
2023-12-03 17:52:24 +1300

    change colours to match figma

--- client/components/UI/NavButton/NavButton.tsx
+++ client/components/UI/NavButton/NavButton.tsx
@@ -10,7 +10,7 @@ function NavButton({ children, ...rest }: ButtonProps) {
   return (
     <button
       className={
-        'bg-lightPurple text-black font-bold py-4 px-4 rounded-full focus:shadow-[0px_0px_5px_2px_#0A0047] w-20 h-20'
+        'bg-slate-300 text-black font-bold py-4 px-4 rounded-full focus:bg-darkPurple focus:shadow-[0px_0px_5px_2px_#0A0047] w-20 h-20'
       }
       {...rest}
     >
