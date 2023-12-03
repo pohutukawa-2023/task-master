0fbe6e8d245be59cb4d9c11ead53a59c6809868c
Kirsty Ammundsen <kirstyammundsen@hotmail.com>
2023-12-04 05:30:34 +1300

    remove secrets

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -8,7 +8,7 @@ function Header() {
       <div className="flex justify-between p-6 font-title text-lg">
         <h2 className="text-darkPurple font-title pt-8 text-6xl">Tasks</h2>
         <Logo>
-          <img src="t.svg" alt="Logo for Task Masters" />
+          <img src="/t.svg" alt="Logo for Task Masters" />
         </Logo>
       </div>
     </>
