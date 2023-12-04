e94d4f0cda03786468d64a0b0fe4e7a111803a47
Kirsty Ammundsen <kirstyammundsen@hotmail.com>
2023-12-05 11:05:30 +1300

    Complete stats render

--- client/Pages/ClientLayout.tsx
+++ client/Pages/ClientLayout.tsx
@@ -1,7 +1,6 @@
 import { useAuth0 } from '@auth0/auth0-react'
 import BottomNav from '../components/BottomNav'
 import { Outlet } from 'react-router-dom'
-import Header from '../components/Header'
 
 // Note that you must add the useQuery and UseAuth0 on the page for the isAdmin and isNot Admin functions to work.
 
@@ -17,7 +16,6 @@ export default function ClientLayout() {
       <div className="p-6">
         <Outlet />
       </div>
-
       <BottomNav />
     </>
   )
