58d53bd42217e722e0d0149edf910c0c3576c234
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-12-06 11:46:40 +1300

    Add conditional dotenv config

--- server/auth0.ts
+++ server/auth0.ts
@@ -4,7 +4,10 @@ import express from 'express'
 import * as oidc from 'express-openid-connect'
 import dotenv from 'dotenv'
 
-dotenv.config()
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  const envConfig = dotenv.config()
+  if (envConfig.error) throw envConfig.error
+}
 
 export const oidcConfig: oidc.ConfigParams = {
   authorizationParams: {
