5d89f3e984f0d3bc529ef7ff595a859aa18689ed
Kirsty Ammundsen <kirstyammundsen@hotmail.com>
2023-12-05 22:11:06 +1300

    clean up code and delete unused files

--- client/Pages/AdminStats.tsx
+++ client/Pages/AdminStats.tsx
@@ -1,6 +1,5 @@
 import { useAuth0 } from '@auth0/auth0-react'
 import { useQuery } from '@tanstack/react-query'
-import { getClientStats } from '../apis/client'
 import GraphPage from '../components/GraphPage'
 import Header from '../components/Header'
 import { getAdminClientStats } from '../apis/admin'
