da2714743159c6822c00786c49f51325f48f79eb
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-12-05 16:17:31 +1300

    Group client task view by date

--- client/Pages/clientTasks.tsx
+++ client/Pages/clientTasks.tsx
@@ -2,14 +2,34 @@ import { useAuth0 } from '@auth0/auth0-react'
 import { getClientTasks, taskDone } from '../apis/client'
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { ClientsTask } from '../../models/task'
-import Checkbox from '../components/UI/Checkbox/Checkbox'
-import TextBox from '../components/UI/Textbox/Textbox'
 import Header from '../components/Header'
 // import { User, UserDraft } from '../../types/User'
 
+function TaskItem({ task, handleChangeDone }) {
+  return (
+    <>
+      <div
+        key={task.id}
+        className="flex items-center p-2 px-4 bg-lightPurple text-white border rounded-full focus:shadow-[0px_0px_5px_2px_#C3ACD0] border-transparent placeholder-[#B07CF2] focus:outline-none block w-full sm:text-sm"
+      >
+        <input
+          type="checkbox"
+          name="taskDone"
+          id={task.id.toString()}
+          checked={task.isComplete}
+          onChange={(e) => handleChangeDone(e, task.id)}
+          className="text-darkNavy accent-darkPurple w-5 h-5 mr-2 rounded-full"
+        />
+
+        <label htmlFor={task.id.toString()}>{task.name}</label>
+        <button></button>
+      </div>
+    </>
+  )
+}
+
 function ClientTasks() {
   const { user, isAuthenticated, getAccessTokenSilently } = useAuth0()
-
   const { data, isLoading } = useQuery({
     queryKey: ['tasks'],
     queryFn: async () => {
@@ -61,39 +81,34 @@ function ClientTasks() {
     mutation.mutate({ done: !e.target.checked, task_id, token })
   }
 
+  const rows = []
+  let lastDate = null
+
+  data?.forEach((task: ClientsTask) => {
+    if (task.date !== lastDate) {
+      rows.push(
+        <div className="font-semibold text-center text-xl">
+          {new Date(task.date).toLocaleDateString('en-GB', {
+            weekday: 'short',
+            day: '2-digit',
+            month: 'short',
+          })}
+        </div>
+      )
+    }
+    rows.push(
+      <TaskItem key={task.id} task={task} handleChangeDone={handleChangeDone} />
+    )
+    lastDate = task.date
+  })
+
   return (
     <>
       <div>
         <Header title="Tasks" />
       </div>
       <form>
-        <div className="mb-28 flex flex-col gap-4">
-          {data?.map((task: ClientsTask) => (
-            <div
-              key={task.id}
-              className="flex items-center p-2 px-4 bg-lightPurple text-white border rounded-full focus:shadow-[0px_0px_5px_2px_#C3ACD0] border-transparent placeholder-[#B07CF2] focus:outline-none block w-full sm:text-sm"
-            >
-              <input
-                type="checkbox"
-                name="taskDone"
-                id={task.id.toString()}
-                checked={task.isComplete}
-                onChange={(e) => handleChangeDone(e, task.id)}
-                className="text-darkNavy rounded-full accent-darkPurple w-5 h-5 mr-2"
-              />
-
-              <label htmlFor={task.id.toString()}>
-                {task.name} -{' '}
-                {new Date(task.date).toLocaleDateString('en-GB', {
-                  weekday: 'short',
-                  day: '2-digit',
-                  month: 'short',
-                })}
-              </label>
-              <button></button>
-            </div>
-          ))}
-        </div>
+        <div className="mb-28 flex flex-col gap-4">{rows}</div>
       </form>
     </>
   )
